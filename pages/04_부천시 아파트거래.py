f0b62b809aeef751cc378dba05d1ef4b6a2cb820
bluesunx1004 <122003333+bluesunx1004@users.noreply.github.com>
2024-12-20 11:49:29 +0900

    Update 04_부천시 아파트거래.py

--- pages/04_부천시 아파트거래.py
+++ pages/04_부천시 아파트거래.py
@@ -12,8 +12,8 @@ def load_data():
 
 data = load_data()
 
-st.title('부천시 아파트 거래 분석(2022년 기준), 데이터 출처: 캐글')
-
+st.title('부천시 아파트 거래 분석(2022년 기준)')
+st.write('데이터 출처: 캐글')
 # 1. 가장 거래가 많은 동
 st.header('1. 가장 거래가 많은 동')
 top_dongs = data['동'].value_counts().head(10)
